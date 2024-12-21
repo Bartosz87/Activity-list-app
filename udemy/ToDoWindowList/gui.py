9af316c5ab7213d48d9e668b7c78e8c46bce6c87
Bartosz Zielinski <bar87ziel@gmail.com>
2024-12-21 21:44:32 +0100

    Adding new activities to list stored in file through gui

--- udemy/ToDoWindowList/gui.py
+++ udemy/ToDoWindowList/gui.py
@@ -1,9 +1,23 @@
 import FreeSimpleGUI as sg
+import functions
 
 label = sg.Text("Type in a to-do")
-input_box=sg.InputText(tooltip="Enter todo")
+input_box=sg.InputText(tooltip="Enter todo", key='activity')
 add_button = sg.Button("Add")
 
-window = sg.Window("To-do-App", layout=[[label], [input_box]])
-window.read()
+window = sg.Window("To-do-App", layout=[[label], [input_box,add_button]])
+
+
+while True:
+    event, values = window.read()
+    activities_list =  functions.get_todos_list()
+    activities_list.append(values['activity'])
+    activities_list[-1]= activities_list[-1] +"\n"
+    functions.write_todos(activities_list)
+
+    print(functions.get_todos_list())
+    print(functions.get_todos())
+    break
+
+
 window.close()
